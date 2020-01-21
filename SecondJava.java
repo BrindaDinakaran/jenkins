01c31083cf4e8f33ccfad2bfc4dd93035dc6ca8b
Viswanth Sanagapalli <msanaga1@in.ibm.com>
2020-01-21 16:10:34 +0530

    added empty java file

---
+++ SecondJava.java
@@ -0,0 +1,12 @@
+ Volume in drive C is Windows
+ Volume Serial Number is 469E-24AF
+
+ Directory of C:\Users\ViswanthSanagapalli\Desktop\javaJenkins
+
+21-01-2020  16:07    <DIR>          .
+21-01-2020  16:07    <DIR>          ..
+21-01-2020  16:06               676 Hello.class
+21-01-2020  00:59               152 hello.java
+21-01-2020  16:07                 0 SecondJava.java
+               3 File(s)            828 bytes
+               2 Dir(s)  130,403,909,632 bytes free
